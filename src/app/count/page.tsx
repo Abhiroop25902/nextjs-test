c68bff139c6675de9c1bb72c64cd8e086f5e74f8
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-10-24 20:47:24 +0530

    ADD: added landing page and proper routing

--- src/app/count/page.tsx
+++ src/app/count/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useEffect, useState } from "react";
-import CircularProgressBar from "../Components/circularProgressBar";
+import { Suspense, useEffect, useState } from "react";
+import CircularProgressBar from "../components/circularProgressBar";
 
 const countLocalStorageKey: string = "COUNT_LOCAL_STORAGE_KEY";
 
@@ -35,23 +35,33 @@ export default function CountingPage() {
 
   if (count === null) {
     return (
-      <CircularProgressBar color="white" heightWidth={50} strokeWidth={2.5} />
+      <div
+        className="grid  min-h-screen  items-center justify-items-center 
+   font-[family-name:var(--font-geist-sans)]"
+      >
+        <CircularProgressBar color="white" heightWidth={50} strokeWidth={2.5} />
+      </div>
     );
   }
 
   return (
-    <main className="flex flex-col gap-8 items-center">
-      <h1 className="text-5xl font-bold">Clicking Test</h1>
-      <p>You have clicked the button {count} times.</p>
-      <button
-        className="inline-flex items-center rounded-md bg-indigo-600 
+    <div
+      className="grid  min-h-screen  items-center justify-items-center 
+     font-[family-name:var(--font-geist-sans)]"
+    >
+      <main className="flex flex-col gap-8 items-center">
+        <h1 className="text-5xl font-bold">Clicking Test</h1>
+        <p>You have clicked the button {count} times.</p>
+        <button
+          className="inline-flex items-center rounded-md bg-indigo-600 
         px-3 py-2  font-semibold text-white hover:bg-indigo-500 
         focus-visible:outline focus-visible:outline-2 
         focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        onClick={incrementCount}
-      >
-        Button
-      </button>
-    </main>
+          onClick={incrementCount}
+        >
+          Button
+        </button>
+      </main>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-import CountingPage from "./examplePages/countingPage";
+import { useRouter } from "next/navigation";
+import RedirectButton from "./components/redirectButton";
 
 export default function Page() {
   return (
     <div
-      className="grid  min-h-screen  items-center justify-items-center 
+      className="flex min-h-screen items-center justify-center 
      font-[family-name:var(--font-geist-sans)]"
     >
-      <CountingPage></CountingPage>
+      <div className="grid grid-cols-3">
+        <h1 className="col-span-3 items-center text-center text-5xl font-bold pb-10">
+          Hello
+        </h1>
+        <div className="col-span-3 flex justify-center">
+          <RedirectButton url="/count" text="Count" />
+        </div>
+      </div>
     </div>
   );
 }
