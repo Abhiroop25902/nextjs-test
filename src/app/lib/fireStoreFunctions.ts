3f6d49fb52be3b52c91fbe4bcfbaf0267a836651
Abhiroop25902 <abhiroop.m25902@gmail.com>
2025-01-05 16:48:31 +0530

    FIX: many css things does not work in gmail, so content changed to basics

--- src/app/lib/fireStoreFunctions.ts
+++ src/app/lib/fireStoreFunctions.ts
@@ -33,24 +33,18 @@ export async function authUsersCount() {
 
 export async function sendActivationEmail(email: string, userId: string): Promise<void> {
     try {
+        const verifyUrl = `https://abhiroop.dev/api/auth/${userId}/verifyEmail`;
+
         const emailData: EmailData = {
             to: [email],
             message: {
                 subject: 'Email Verification for abhiroop.dev',
-                html: `<div style="display: flex;justify-content: center;background-color:#efefef; height:100vh; align-items: center">
-                            <div style="width: 500rem;margin-left: 15rem; margin-right: 15rem; border-radius: 20px; background-color: white;border: 1px black solid; height:200px">
-                                <div style="display: flex; flex-direction: column; align-items: center; font-family: system-ui">
-                                    <h1 style="margin-block-end:0.5rem">Verify Email</h1>
-                                    <p style="padding-left: 5rem; padding-right: 5rem; text-align: center">
-                                        Please confirm that you want to use this email, clicking the button below will enable your access for <a href="https://abhiroop.dev">abhiroop.dev</a>
-                                    </p>
-                                    <button 
-                                        style="background-color: #0064d7; color: white; border-radius: 10px; padding-left: 1rem; padding-right: 1rem; border: none; height:2.5rem; font-size: 0.875rem; line-height: 1.25rem"
-                                        onclick="window.open('https://abhiroop.dev/api/auth/${userId}/verifyEmail')"
-                                    >Confirm Email</button>
-                                </div>
-                            </div>
-                        </div>`
+                html: `<div>
+                            <p style="font-family:system-ui"> 
+                                Please confirm that you want to use this email, clicking the button below will enable your access for <a href="https://abhiroop.dev">abhiroop.dev</a>
+                            </p>
+                            <a href="${verifyUrl}" target="_blank" style="font-family:system-ui">Verify Email</a>
+                       </div>`
             }
         }
         await db.collection(MAIL_COLLECTION_NAME).add(emailData);
