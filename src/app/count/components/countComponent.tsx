0e9cc3892591c2cb4c7254eeb41354b6da2b0738
abhiroop25902 <abhiroop.m25902@gmail.com>
2025-05-08 01:34:06 +0530

    REFAC: refactored countComponent + made LocalStorageKeys.ts to store localStorage keys

--- src/app/count/components/countComponent.tsx
+++ src/app/count/components/countComponent.tsx
@@ -1,54 +1,56 @@
 "use client";
 
 import CircularProgressBar from "@/app/Components/circularProgressBar";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
+import LocalStorageKeys from "@/constants/LocalStorageKeys";
 
-const countLocalStorageKey: string = "COUNT_LOCAL_STORAGE_KEY";
 
 export default function CountComponent() {
-  const getInitialCount = () => {
-    const storedCount = localStorage.getItem(countLocalStorageKey) ?? "0";
+    const {countLocalStorageKey} = LocalStorageKeys;
+    
+    const [count, setCount] = useState<number | null>(null);
 
-    return parseInt(storedCount);
-  };
+    useEffect(() => {
+        const getInitialCount = () => {
+            const storedCount = localStorage.getItem(countLocalStorageKey) ?? "0";
 
-  const [count, setCount] = useState<number | null>(null);
+            return parseInt(storedCount);
+        };
 
-  useEffect(() => {
-    setTimeout(() => {
-      const initialCount = getInitialCount();
+        setTimeout(() => {
+            const initialCount = getInitialCount();
 
-      setCount(initialCount);
-    }, 2000);
-  }, []);
+            setCount(initialCount);
+        }, 2000);
+    }, [countLocalStorageKey]);
 
-  useEffect(() => {
-    if (count != null) {
-      localStorage.setItem(countLocalStorageKey, count.toString());
+    useEffect(() => {
+        if (count != null) {
+            localStorage.setItem(countLocalStorageKey, count.toString());
+        }
+    }, [count, countLocalStorageKey]);
+
+    const incrementCount = function () {
+        if (count != null) {
+            setCount(count + 1);
+        }
+    };
+
+    if (count === null) {
+        return <CircularProgressBar heightWidth={50} strokeWidth={2.5}/>;
     }
-  }, [count]);
-
-  const incrementCount = function () {
-    if (count != null) {
-      setCount(count + 1);
-    }
-  };
-
-  if (count === null) {
-    return <CircularProgressBar heightWidth={50} strokeWidth={2.5} />;
-  }
-  return (
-    <>
-      <p>You have clicked the button {count} times.</p>
-      <button
-        className="inline-flex items-center rounded-md bg-indigo-600 
+    return (
+        <>
+            <p>You have clicked the button {count} times.</p>
+            <button
+                className="inline-flex items-center rounded-md bg-indigo-600
     px-3 py-2  font-semibold text-white hover:bg-indigo-500 
     focus-visible:outline focus-visible:outline-2 
     focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        onClick={incrementCount}
-      >
-        Button
-      </button>
-    </>
-  );
+                onClick={incrementCount}
+            >
+                Button
+            </button>
+        </>
+    );
 }
---
+++ src/constants/LocalStorageKeys.ts
@@ -0,0 +1,6 @@
+const LocalStorageKeys: { [key: string]: string } = {
+    countLocalStorageKey: 'COUNT_LOCAL_STORAGE_KEY'
+}
+
+export default LocalStorageKeys
+
