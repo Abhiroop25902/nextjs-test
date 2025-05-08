b7d8f8826b4763b09c16a249ead845501ddb7603
abhiroop25902 <abhiroop.m25902@gmail.com>
2025-05-09 02:02:37 +0530

    ADD: added types and enum related to chat message

--- src/app/queryai/types.ts
+++ src/app/queryai/types.ts
@@ -1,4 +1,13 @@
+import {MessageRole} from "@/app/queryai/enums";
+
 export type ApiKeyDialogHandles = {
     showModal: () => void;
     hideModal: () => void;
+}
+
+export type ChatMessage = {
+    id: string,
+    role: MessageRole,
+    text: string,
+    display: boolean
 }
