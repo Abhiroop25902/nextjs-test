c7fb0f8a57eb04affa64e9bc8f0d039318afe55d
abhiroop25902 <abhiroop.m25902@gmail.com>
2025-05-10 01:34:02 +0530

    ADD: added chart to display after user enters anything, will be modified to automated detection and update of chartOption

--- src/app/queryai/Components/TextArea.tsx
+++ src/app/queryai/Components/TextArea.tsx
@@ -8,6 +8,8 @@ import LocalStorageKeys from "@/app/constants/LocalStorageKeys";
 import {chatHistoryStore} from "@/app/queryai/stores/chatHistoryStore";
 import {MessageRole} from "@/app/queryai/enums";
 import {v4 as uuidv4} from 'uuid';
+import {chartOptionStore} from "@/app/queryai/stores/chartOptionStore";
+import Highcharts from "highcharts";
 
 export default function TextArea() {
     const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
@@ -18,6 +20,7 @@ export default function TextArea() {
     }
 
     const pushMessage = chatHistoryStore(store => store.pushMessage);
+    const updateChartOption = chartOptionStore(store => store.updateChartOption)
 
     function handleInput() {
         const messageText = textAreaRef.current?.value
@@ -31,8 +34,43 @@ export default function TextArea() {
             }
 
             pushMessage(message);
+            //todo: remove it after adding detection part
+            const chartComponentOptions: Highcharts.Options = {
+                chart: {
+                    type: 'line',
+                    renderTo: 'container',
+                    backgroundColor: '#00000000', // Dark background
+                },
+                title: {
+                    text: 'Sample Highcharts Execution',
+                    style: {color: '#FFFFFF'}, // White title
+                },
+                xAxis: {
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+                    labels: {style: {color: '#FFFFFF'}}, // White labels
+                },
+                yAxis: {
+                    title: {
+                        text: 'Values',
+                        style: {color: '#FFFFFF'}, // White labels
+                    },
+                    labels: {style: {color: '#FFFFFF'}},
+                },
+                series: [
+                    {
+                        name: 'Example Series',
+                        type: 'line',
+                        data: [10, 20, 30, 40, 50],
+                        color: '#FFCC00', // Bright color for visibility
+                    },
+                ],
+                legend: {
+                    itemStyle: {color: '#FFFFFF'}, // White legend text
+                },
+            };
+            updateChartOption(chartComponentOptions)
         }
-        
+
         textAreaRef.current!.value = "";
     }
 
